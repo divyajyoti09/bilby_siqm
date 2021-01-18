9a3219864d79744fd0ffb22842912119b977a6b4
Aditya Vijaykumar <vijaykumar.aditya@gmail.com>
2021-01-18 20:50:23 +0530

    correct spurious and obvious changes

--- bilby/gw/utils.py
+++ bilby/gw/utils.py
@@ -957,7 +957,6 @@ def lalsim_SimInspiralWaveformParamsInsertTidalLambda1(
         waveform_dictionary, lambda_1)
 
 
-
 def lalsim_SimInspiralWaveformParamsInsertdQuadMon1(
         waveform_dictionary, dQuadMon1):
     try:
@@ -969,7 +968,6 @@ def lalsim_SimInspiralWaveformParamsInsertdQuadMon1(
         waveform_dictionary, dQuadMon1)
 
 
-
 def lalsim_SimInspiralWaveformParamsInsertdQuadMon2(
         waveform_dictionary, dQuadMon2):
     try:
@@ -979,6 +977,7 @@ def lalsim_SimInspiralWaveformParamsInsertdQuadMon2(
 
     return lalsim.SimInspiralWaveformParamsInsertdQuadMon2(
         waveform_dictionary, dQuadMon2)
+
 
 def lalsim_SimInspiralWaveformParamsInsertTidalLambda2(
         waveform_dictionary, lambda_2):
