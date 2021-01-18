f7893e7b40ca5f69be98c243646bc1cfec36a6a8
Aditya Vijaykumar <vijaykumar.aditya@gmail.com>
2021-01-18 20:50:23 +0530

    correct spurious and obvious changes

--- examples/gw_examples/injection_examples/lal_siqm_example.py
+++ examples/gw_examples/injection_examples/lal_siqm_example.py
@@ -28,7 +28,24 @@ np.random.seed(88170235)
 # aligned spins of both black holes (chi_1, chi_2), etc.
 # dQuadMon1 =0  and dQuadMon2=0 will give BH signal
 
-injection_parameters = dict(mass_1=20., mass_2=10., a_1=0.7, a_2=0.3, tilt_1=0.5, tilt_2=1.0,phi_12=1.7, phi_jl=0.3, luminosity_distance=200., theta_jn=0.4, psi=2.659,phase=1.3, geocent_time=1126259642.413, ra=1.375, dec=-1.2108, dQuadMon1=20,,dQuadMon2=0)
+injection_parameters = dict(
+    mass_1=20.,
+    mass_2=10.,
+    a_1=0.7,
+    a_2=0.3,
+    tilt_1=0.5,
+    tilt_2=1.0,
+    phi_12=1.7,
+    phi_jl=0.3,
+    luminosity_distance=200.,
+    theta_jn=0.4,
+    psi=2.659,
+    phase=1.3,
+    geocent_time=1126259642.413,
+    ra=1.375,
+    dec=-1.2108,
+    dQuadMon1=20,
+    dQuadMon2=0)
 
 # Set the duration and sampling frequency of the data segment that we're going
 # to inject the signal into. For the
@@ -39,30 +56,37 @@ start_time = injection_parameters['geocent_time'] + 2 - duration
 
 # Fixed arguments passed into the source model. The analysis starts at 40 Hz.
 waveform_arguments = dict(waveform_approximant='IMRPhenomPv2',
-                                  reference_frequency=50., minimum_frequency=40.)
+                          reference_frequency=50., minimum_frequency=40.)
 
 
 # Create the waveform_generator using a LAL SIQM  source function
-waveform_generator = bilby.gw.WaveformGenerator(duration=duration, sampling_frequency=sampling_frequency,frequency_domain_source_model=bilby.gw.source.lal_siqm,parameter_conversion=bilby.gw.conversion.convert_to_lal_siqm_parameters,waveform_arguments=waveform_arguments)
+waveform_generator = bilby.gw.WaveformGenerator(
+    duration=duration,
+    sampling_frequency=sampling_frequency,
+    frequency_domain_source_model=bilby.gw.source.lal_siqm,
+    parameter_conversion=bilby.gw.conversion.convert_to_lal_siqm_parameters,
+    waveform_arguments=waveform_arguments)
 # (LIGO-Hanford (H1), LIGO-Livingston (L1), and Virgo (V1)).
 # These default to their design sensitivity and start at 40 Hz.
 
 ifos = bilby.gw.detector.InterferometerList(['H1', 'L1'])
-ifos.set_strain_data_from_power_spectral_densities(sampling_frequency=sampling_frequency, duration=duration,start_time=injection_parameters['geocent_time'] - 3)
-ifos.inject_signal(waveform_generator=waveform_generator,parameters=injection_parameters)
+ifos.set_strain_data_from_power_spectral_densities(
+    sampling_frequency=sampling_frequency, duration=duration, start_time=injection_parameters['geocent_time'] - 3)
+ifos.inject_signal(waveform_generator=waveform_generator, parameters=injection_parameters)
 
 priors = bilby.gw.prior.BBHPriorDict()
 priors['geocent_time'] = bilby.core.prior.Uniform(
-            minimum=injection_parameters['geocent_time'] - 1,
-                maximum=injection_parameters['geocent_time'] + 1,
-                    name='geocent_time', latex_label='$t_c$', unit='$s$')
-for key in ['a_1', 'a_2', 'tilt_1', 'tilt_2', 'phi_12', 'phi_jl', 'psi', 'ra','dec', 'geocent_time', 'phase']:
+    minimum=injection_parameters['geocent_time'] - 1,
+    maximum=injection_parameters['geocent_time'] + 1,
+    name='geocent_time', latex_label='$t_c$', unit='$s$')
+for key in ['a_1', 'a_2', 'tilt_1', 'tilt_2', 'phi_12', 'phi_jl', 'psi', 'ra', 'dec', 'geocent_time', 'phase']:
     priors[key] = injection_parameters[key]
 priors['dQuadMonS'] = bilby.core.prior.Uniform(0, 500, name='dQuadMonS')
 priors['dQuadMonA'] = 0
 likelihood = bilby.gw.GravitationalWaveTransient(interferometers=ifos, waveform_generator=waveform_generator)
 
-result = bilby.run_sampler(likelihood=likelihood, priors=priors, sampler='dynesty', npoints=1000,injection_parameters=injection_parameters, outdir=outdir, label=label)
+result = bilby.run_sampler(likelihood=likelihood, priors=priors, sampler='dynesty', npoints=1000,
+                           injection_parameters=injection_parameters, outdir=outdir, label=label)
 
 # Make a corner plot.
 result.plot_corner()
