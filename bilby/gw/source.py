fd78782aa0531eeb56edecf6b987292f33fd5642
Nv Krishnendu <nv.krishnendu@holodeck1>
2021-07-01 11:31:24 +0000

    changes to kappa-lambda

--- bilby/gw/source.py
+++ bilby/gw/source.py
@@ -8,6 +8,8 @@ from .utils import (lalsim_GetApproximantFromString,
                     lalsim_SimInspiralChooseFDWaveform,
                     lalsim_SimInspiralWaveformParamsInsertTidalLambda1,
                     lalsim_SimInspiralWaveformParamsInsertTidalLambda2,
+                    lalsim_SimInspiralWaveformParamsInsertdQuadMon1,
+                    lalsim_SimInspiralWaveformParamsInsertdQuadMon2,
                     lalsim_SimInspiralChooseFDWaveformSequence)
 
 try:
@@ -197,7 +199,97 @@ def lal_binary_black_hole(
         phi_jl=phi_jl, **waveform_kwargs)
 
 
+
 def lal_binary_neutron_star(
+        frequency_array, mass_1, mass_2, luminosity_distance, a_1, tilt_1,
+        phi_12, a_2, tilt_2, phi_jl, theta_jn, phase, lambda_1, lambda_2,
+        **kwargs):
+    """ A Binary Neutron Star waveform model using lalsimulation
+
+    Parameters
+    ----------
+    frequency_array: array_like
+        The frequencies at which we want to calculate the strain
+    mass_1: float
+        The mass of the heavier object in solar masses
+    mass_2: float
+        The mass of the lighter object in solar masses
+    luminosity_distance: float
+        The luminosity distance in megaparsec
+    a_1: float
+        Dimensionless primary spin magnitude
+    tilt_1: float
+        Primary tilt angle
+    phi_12: float
+        Azimuthal angle between the two component spins
+    a_2: float
+        Dimensionless secondary spin magnitude
+    tilt_2: float
+        Secondary tilt angle
+    phi_jl: float
+        Azimuthal angle between the total binary angular momentum and the
+        orbital angular momentum
+    theta_jn: float
+        Orbital inclination
+    phase: float
+        The phase at coalescence
+    lambda_1: float
+        Dimensionless tidal deformability of mass_1
+    lambda_2: float
+        Dimensionless tidal deformability of mass_2
+    kwargs: dict
+        Optional keyword arguments
+        Supported arguments:
+            waveform_approximant
+            reference_frequency
+            minimum_frequency
+            maximum_frequency
+            catch_waveform_errors
+            pn_spin_order
+            pn_tidal_order
+            pn_phase_order
+            pn_amplitude_order
+            mode_array:
+                Activate a specific mode array and evaluate the model using those
+                modes only.  e.g. waveform_arguments =
+                dict(waveform_approximant='IMRPhenomHM', mode_array=[[2,2],[2,-2])
+                returns the 22 and 2-2 modes only of IMRPhenomHM.  You can only
+                specify modes that are included in that particular model.  e.g.
+                waveform_arguments = dict(waveform_approximant='IMRPhenomHM',
+                mode_array=[[2,2],[2,-2],[5,5],[5,-5]]) is not allowed because the
+                55 modes are not included in this model.  Be aware that some models
+                only take positive modes and return the positive and the negative
+                mode together, while others need to call both.  e.g.
+                waveform_arguments = dict(waveform_approximant='IMRPhenomHM',
+                mode_array=[[2,2],[4,-4]]) returns the 22 a\nd 2-2 of IMRPhenomHM.
+                However, waveform_arguments =
+                dict(waveform_approximant='IMRPhenomXHM', mode_array=[[2,2],[4,-4]])
+                returns the 22 and 4-4 of IMRPhenomXHM.
+
+    Returns
+    -------
+    dict: A dictionary with the plus and cross polarisation strain modes
+    """
+    waveform_kwargs = dict(
+        waveform_approximant='IMRPhenomPv2_NRTidal', reference_frequency=50.0,
+        minimum_frequency=20.0, maximum_frequency=frequency_array[-1],
+        catch_waveform_errors=False, pn_spin_order=-1, pn_tidal_order=-1,
+        pn_phase_order=-1, pn_amplitude_order=0)
+    waveform_kwargs.update(kwargs)
+    return _base_lal_cbc_fd_waveform(
+        frequency_array=frequency_array, mass_1=mass_1, mass_2=mass_2,
+        luminosity_distance=luminosity_distance, theta_jn=theta_jn, phase=phase,
+        a_1=a_1, a_2=a_2, tilt_1=tilt_1, tilt_2=tilt_2, phi_12=phi_12,
+        phi_jl=phi_jl, lambda_1=lambda_1, lambda_2=lambda_2, **waveform_kwargs)
+
+
+def dquadmon_from_lambda(xx):
+    prefact1 = 0.61
+    prefact2 = 0.3
+    dQuadMon = prefact1 + prefact2 * np.log(xx)
+    return dQuadMon
+
+def lal_binary_neutron_star_NEW(
         frequency_array, mass_1, mass_2, luminosity_distance, a_1, tilt_1,
         phi_12, a_2, tilt_2, phi_jl, theta_jn, phase, lambda_1, lambda_2,
         **kwargs):
@@ -234,6 +326,7 @@ def lal_binary_neutron_star(
         Dimensionless tidal deformability of mass_1
     lambda_2: float
         Dimensionless tidal deformability of mass_2
+
     kwargs: dict
         Optional keyword arguments
         Supported arguments:
@@ -269,10 +362,19 @@ def lal_binary_neutron_star(
     dict: A dictionary with the plus and cross polarisation strain modes
     """
     waveform_kwargs = dict(
-        waveform_approximant='IMRPhenomPv2_NRTidal', reference_frequency=50.0,
+        waveform_approximant='TaylorF2', reference_frequency=50.0,
         minimum_frequency=20.0, maximum_frequency=frequency_array[-1],
         catch_waveform_errors=False, pn_spin_order=-1, pn_tidal_order=-1,
         pn_phase_order=-1, pn_amplitude_order=0)
+
+    wf_dict = waveform_kwargs.get("lal_waveform_dictionary", lal.CreateDict())
+
+
+    dQuadMon1 = dquadmon_from_lambda(lambda_1);
+    dQuadMon2 = dquadmon_from_lambda(lambda_2);
+    lalsim.SimInspiralWaveformParamsInsertdQuadMon1(wf_dict, dQuadMon1);
+    lalsim.SimInspiralWaveformParamsInsertdQuadMon2(wf_dict, dQuadMon2);
+
     waveform_kwargs.update(kwargs)
     return _base_lal_cbc_fd_waveform(
         frequency_array=frequency_array, mass_1=mass_1, mass_2=mass_2,
@@ -393,6 +495,7 @@ def _base_lal_cbc_fd_waveform(
     =======
     dict: A dictionary with the plus and cross polarisation strain modes
     """
+    import lal
     waveform_approximant = waveform_kwargs['waveform_approximant']
     reference_frequency = waveform_kwargs['reference_frequency']
     minimum_frequency = waveform_kwargs['minimum_frequency']
@@ -443,6 +546,7 @@ def _base_lal_cbc_fd_waveform(
         waveform_dictionary, lambda_1)
     lalsim_SimInspiralWaveformParamsInsertTidalLambda2(
         waveform_dictionary, lambda_2)
+    
 
     for key, value in waveform_kwargs.items():
         func = getattr(lalsim, "SimInspiralWaveformParamsInsert" + key, None)
